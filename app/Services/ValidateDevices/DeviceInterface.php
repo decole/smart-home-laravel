75a469a73360c7d6becb96b8917f609840e493dd
decole <decole@rambler.ru>
2020-04-26 01:39:32 +0400

    add interface of devices

---
+++ app/Services/ValidateDevices/DeviceInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Services\ValidateDevices;
+
+
+interface DeviceInterface
+{
+
+    /**
+     * Передача массива топиков
+     * @return array
+     */
+    public function getTopics();
+
+    /**
+     * Создание кэшированных данных
+     * @return array
+     */
+    public function createDataset();
+
+    /**
+     * Анализ приходящих сообщений на вхождение в допустимые прределы**
+     * @param $message
+     * @return mixed
+     */
+    public function deviceValidate($message);
+
+}
