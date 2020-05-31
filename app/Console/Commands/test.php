25db0d00000b1fbbd74996add999350f95f95a8b
Sergy <decole@rambler.ru>
2020-06-01 00:07:52 +0400

    test component command

--- app/Console/Commands/test.php
+++ app/Console/Commands/test.php
@@ -2,18 +2,18 @@
 
 namespace App\Console\Commands;
 
-use App\AliceSecure;
-use App\Jobs\SendEmail;
-use App\Jobs\SendTelegramNotify;
-use App\Notifications\SensorNotify;
-use App\Notifications\UserNotify;
-use App\User;
-use App\Weather;
+
+use App\MqttHistory;
+use App\MqttHistoryFireSecure;
+use App\MqttHistorySecure;
+use App\MqttHistoryWatering;
+use App\MqttSensor;
+use App\Notification;
 use DateInterval;
 use DateTime;
 use Illuminate\Console\Command;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Notification;
+
 use phpDocumentor\Reflection\Types\Object_;
 
 class test extends Command
@@ -50,6 +50,37 @@ class test extends Command
      */
     public function handle()
     {
+        /*
+        $this->info('start after 3 month clear');
+        Notification::whereDate('created_at', '<', (new \Carbon\Carbon)->submonths(1) )->each(function ($item) {
+                $item->delete();
+            });
+        $this->info('Notify: clear');
+        MqttHistory::whereDate('created_at', '<', (new \Carbon\Carbon)->submonths(3) )->each(function ($item) {
+            $item->delete();
+        });
+        $this->info('MqttHistory: clear');
+        MqttHistoryWatering::whereDate('created_at', '<', (new \Carbon\Carbon)->submonths(3) )->each(function ($item) {
+            $item->delete();
+        });
+        $this->info('MqttHistoryWatering: clear');
+        MqttHistorySecure::whereDate('created_at', '<', (new \Carbon\Carbon)->submonths(3) )->each(function ($item) {
+            $item->delete();
+        });
+        $this->info('MqttHistorySecure: clear');
+        MqttHistoryFireSecure::whereDate('created_at', '<', (new \Carbon\Carbon)->submonths(3) )->each(function ($item) {
+            $item->delete();
+        });
+        $this->info('MqttHistoryFireSecure: clear');
+        */
+        /**
+          public function handle()
+          {
+              Item::where('datetime', '<', Carbon::now())->each(function ($item) {
+                   $item->delete();
+            });
+         }
+         */
         /*
         $this->info('test registring user in AliceSecure');
         $id = '13D65C01F8B51512AF66DAC3DCAE2F893A9D3E8B0851A6BF9C44EB512D48F065';
